8ed44a875919eb60e5c82146d7eac1f2276745da
Bob Dröge <b.e.droge@rug.nl>
2021-03-12 15:12:53 +0100

    add openstack provider + resources

--- dynamic/terraform/main.tf
+++ dynamic/terraform/main.tf
@@ -41,3 +41,35 @@ resource "aws_security_group_rule" "instance_open_public" {
   protocol  = "tcp"
 }
 
+terraform {
+required_version = ">= 0.14.0"
+  required_providers {
+    openstack = {
+      source  = "terraform-provider-openstack/openstack"
+      version = "~> 1.35.0"
+    }
+  }
+}
+
+provider "openstack" {
+}
+
+resource "openstack_networking_secgroup_v2" "instance" {
+  name = "${var.localuser}-eessi-security"
+}
+
+resource "openstack_networking_secgroup_rule_v2" "instance_open_public" {
+  direction         = "ingress"
+  ethertype         = "IPv4"
+  protocol          = "tcp"
+  port_range_min    = 22
+  port_range_max    = 22
+  remote_ip_prefix  = "0.0.0.0/0"
+  #security_group_id = "${openstack_networking_secgroup_v2.instance.id}"
+  security_group_id = openstack_networking_secgroup_v2.instance.id
+}
+
+resource "openstack_compute_keypair_v2" "deployer" {
+  name       = "${var.localuser}-deployer-key"
+  public_key = file(var.keys["public"])
+}
