8e441365f9e8a3c7dc98e0fd6abc6c23665df308
Bob Dröge <b.e.droge@rug.nl>
2021-03-12 16:06:20 +0100

    use data variable for image id

--- dynamic/terraform/platform_ppc64le.tf
+++ dynamic/terraform/platform_ppc64le.tf
@@ -1,6 +1,6 @@
 resource "openstack_compute_instance_v2" "infra-ppc64le" {
   name            = "eessi-ppc64le"
-  image_id        = "1629a150-4dff-4be3-97b7-01c5ec21f01f" # CentOS 8.3 LE
+  image_id        = data.openstack_images_image_v2.ppc64le.id
   flavor_name     = var.instance_ppc64le[var.mode]
   security_groups = [openstack_networking_secgroup_v2.instance.id]
   key_pair        = "${var.localuser}-deployer-key"
